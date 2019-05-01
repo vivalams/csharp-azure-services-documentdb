df28fcce5939f998c9c26b3b33358e723be6ee4a
Vijay Valaboju <vivala@microsoft.com>
2019-05-01 12:25:17 -0700

    Updated test Project and modified nuget packages

---
+++ {{ cookiecutter.assembly_name }}/{{ cookiecutter.assembly_name }}.Test/TestBase.cs
@@ -0,0 +1,23 @@
+// -----------------------------------------------------------------------
+// 
+// <copyright file="TestBase.cs" company="Microsoft">
+// 
+// Copyright (c) Microsoft. All rights reserved.
+// 
+// </copyright>
+// 
+// -----------------------------------------------------------------------
+namespace {{ cookiecutter.assembly_name }}.Api.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Class TestBase. Holds the utility methods for the tests
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class TestBase
+    { }
+
+}
